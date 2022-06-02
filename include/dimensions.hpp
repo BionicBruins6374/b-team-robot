7c3dd27dfad8a5a0a2ee97bff52b564f8ae99673
Matt Fellenz <matt@felle.nz>
2022-06-02 16:36:27 -0700

    Sort and clean up constants

--- include/dimensions.hpp
+++ include/dimensions.hpp
@@ -3,8 +3,8 @@
 // namespace for phsyical robot and field dimensions
 namespace dimensions {
 constexpr float GOAL_HEIGHT = 63.5;  // 25 in to cm
-constexpr float SENSOR_HEIGHT = 44.45;  // 17.5 in to cm
-constexpr float LENS_ANGLE = 0.261799;  // 15 deg to radians
-constexpr float LAUNCH_ANGLE = 0.785398;  // 45 deg to radians
-constexpr float LAUNCH_HEIGHT = 38.1;  // 15 in to cm
+constexpr float LAUNCH_ANGLE = 0.785398f;  // 45 deg to radians
+constexpr float LAUNCH_HEIGHT = 38.1f;  // 15 in to cm
+constexpr float LENS_ANGLE = 0.261799f;  // 15 deg to radians
+constexpr float SENSOR_HEIGHT = 44.45f;  // 17.5 in to cm
 }  // namespace dimensions
--- include/ports.hpp
+++ include/ports.hpp
@@ -5,11 +5,14 @@
 // namespace for port connections to V5 devices
 namespace ports {
 constexpr uint8_t VISION = 1;
+
 constexpr uint8_t FLYWHEEL_LEFT = 2;
 constexpr uint8_t FLYWHEEL_RIGHT = 3;
+
 constexpr uint8_t PISTON_INDEXER = 'A';
+
 constexpr uint8_t LEFT_BACK_MOTOR = 4;
-constexpr uint8_t RIGHT_BACK_MOTOR = 4;
-constexpr uint8_t LEFT_FRONT_MOTOR = 4;
-constexpr uint8_t RIGHT_FRONT_MOTOR = 4;
+constexpr uint8_t RIGHT_BACK_MOTOR = 5;
+constexpr uint8_t LEFT_FRONT_MOTOR = 6;
+constexpr uint8_t RIGHT_FRONT_MOTOR = 7;
 }  // namespace ports
