2d6f19fda611aec739b05d5c56ef97b81ffef580
seadeef <kevin@drymail.com>
2022-07-11 22:00:20 -0700

    Update constant values to reflect new conditions

--- include/dimensions.hpp
+++ include/dimensions.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-// namespace for phsyical robot and field dimensions
+// namespace for physical robot and field dimensions
 namespace dimensions {
-constexpr float GOAL_HEIGHT = 63.5;  // 25 in to cm
+constexpr float GOAL_HEIGHT = 74.93f;  // 29.5 in to cm
 constexpr float LAUNCH_ANGLE = 0.785398f;  // 45 deg to radians
 constexpr float LAUNCH_HEIGHT = 38.1f;  // 15 in to cm
-constexpr float LENS_ANGLE = 0.261799f;  // 15 deg to radians
-constexpr float SENSOR_HEIGHT = 44.45f;  // 17.5 in to cm
+constexpr float LENS_ANGLE = 0.4276f;  // 24.5 deg to radians
+constexpr float SENSOR_HEIGHT = 43.18f;  // 17 in to cm
 }  // namespace dimensions
