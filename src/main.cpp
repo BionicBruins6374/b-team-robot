e2deaca03f306d73d1c3796c2099d9cbc5345c94
zoya <zdzhnou@gmail.com>
2022-10-29 05:52:05 -0700

    Untested auton w/ Okapi

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "pros/rtos.hpp"
 #include "pros/vision.hpp"
 
+#include "Drivetrain.hpp"
 #include "Flywheel.hpp"
 #include "Robot.hpp"
 #include "Intake.hpp"
@@ -33,11 +34,102 @@ void opcontrol() {
 	}
 }
 
+void low_goals(int disk_num, Flywheel a_flywheel) {
+
+	// Turns on flywheel but doesn't let it fully 
+	// -> it won't shoot accurately/into high goals
+	a_flywheel.toggle_active(true);
+	pros::Task::delay(2000);
+
+	// Loops through numbers of disks: 
+	for (int i = 0 ; i < disk_num ; i ++) {
+		// Shoots disk and lets flywheel recover 
+		a_flywheel.shoot();
+		pros::Task::delay(1500);		
+	}
+}
+
+void spin_rollers_v1(Drivetrain a_drivetrain, Roller a_roller) {
+	// Moves bot forward at fastest speed
+	a_drivetrain.update(127, 0); 
+	pros::Task::delay(500);
+
+	// Drivetrain stops moving
+	a_drivetrain.update(0, 0); 
+
+	// Spin rollers
+	a_roller.switch_color();
+	
+	pros::Task::delay(10); // transition phase
+
+	// Moves the drivetrain backwards for 0.1 seconds
+	a_drivetrain.update(-127,0); 
+	pros::Task::delay(100);
+
+	// Stops moving drivetrain
+	a_drivetrain.update(0,0); 
+}
+
+void purple_rollers_high(std::shared_ptr<OdomChassisController> a_odom, Drivetrain drivetrain, Flywheel a_flywheel, Roller a_roller) {
+	// Do rollers
+	spin_rollers_v1(drivetrain, a_roller);
+	// Turn on flywheel
+	a_flywheel.toggle_active(true);
+	// Move backward 3 TILES - 42 cm
+	a_odom->moveDistance(-1 * 3_ft * TILE_LENGTH_FT - 1.37795_ft);
+	// Turn 90 clockwise
+	a_odom->turnAngle(90_deg);
+	// Forward 1 tile
+	a_odom->moveDistance(1_ft * TILE_LENGTH_FT);
+	// Turn 135 counter clockwise
+	a_odom->turnAngle(-135_deg);
+	// Shoots the disks out of the flywheel twice, 5.5 seconds apart
+	a_flywheel.shoot();
+	pros::Task::delay(5500);
+	a_flywheel.shoot();
+	pros::Task::delay(20);
+}
+
+void green_rollers_low(std::shared_ptr<OdomChassisController> a_odom, Drivetrain a_drivetrain, Flywheel a_flywheel, Roller a_roller) {
+	// Do rollers
+	spin_rollers_v1(a_drivetrain, a_roller);
+	// 5 in back
+	a_odom->moveDistance(0.41667_ft);
+	// 90 deg clockwise
+	a_odom->turnAngle(90_deg);
+	// 3 in + 1.5tiles
+	a_odom->moveDistance(0.25_ft + 1.5_ft * TILE_LENGTH_FT);
+	// Shoot low goals
+	low_goals(2, a_flywheel);
+}
+
+void pink_double_rollers(std::shared_ptr<OdomChassisController> a_odom, Drivetrain a_drivetrain, Roller a_roller) {
+	// Rollers
+	spin_rollers_v1(a_drivetrain, a_roller);
+	// moves back 1 in + 1/2 tile
+	a_odom->moveDistance((TILE_LENGTH_FT * 1_ft + 0.08333_ft));
+	// 90 deg clockwise
+	a_odom->turnAngle(90_deg);
+	// 1 + 1/2 tiles
+	a_odom->moveDistance(1.5_ft * TILE_LENGTH_FT);
+	// Rollers
+	spin_rollers_v1(a_drivetrain, a_roller);
+}
+
 void autonomous() {
 	auto odometry = build_odometry(okapi::MotorGroup({okapi::Motor(-ports::LEFT_BACK_MOTOR), okapi::Motor(-ports::LEFT_FRONT_MOTOR)}), 
 								   okapi::MotorGroup({okapi::Motor(ports::RIGHT_BACK_MOTOR), okapi::Motor(ports::RIGHT_FRONT_MOTOR)}));
-	odometry->turnAngle(45_deg);
-	odometry->moveDistance(0.8333_ft);
+	// Define parts to be manipulated
+	Drivetrain  drivetrain{ ports::LEFT_BACK_MOTOR, 
+	ports::RIGHT_BACK_MOTOR, ports::LEFT_FRONT_MOTOR, 
+	ports::RIGHT_FRONT_MOTOR };
+	Flywheel  flywheel{ ports::FLYWHEEL_LEFT, 
+	ports::FLYWHEEL_RIGHT, ports::PISTON_INDEXER };
+
+
+	// purple_rollers_high(odometry, drivetrain, flywheel);
+	// green_rollers_low(odometry, drivetrain, flywheel);
+	// pink_double_rollers(odometry, drivetrain);
 }
 
 
